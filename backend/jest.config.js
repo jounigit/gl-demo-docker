b91c19bd4023c7b10db46f35ff56844a34ff3728
Jouni Airaksinen <airaksinen.jouni@gmail.com>
2024-03-15 16:06:48 +0200

    master

--- backend/jest.config.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable no-undef */
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  roots: ['<rootDir>/src/tests'],
-}
