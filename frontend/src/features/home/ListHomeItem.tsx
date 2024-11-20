137e0371eec20906d97502756bedb0b5db188ae0
Jouni Airaksinen <airaksinen.jouni@gmail.com>
2024-11-20 11:41:52 +0200

    docker dev related

--- frontend/src/features/home/ListHomeItem.tsx
+++ frontend/src/features/home/ListHomeItem.tsx
@@ -5,6 +5,16 @@ import kuva2 from '../../assets/kuva-2.jpg'
 import kuva3 from '../../assets/kuva-3.jpg'
 import config from "../../data/config";
 import { formatUrl } from "../../components/atoms/utils";
+import { LinkTo } from "../../components/layouts/nav/NavLinks.styles";
+import styled from "styled-components";
+
+const LinkStyle = styled(LinkTo)`
+    display: inline-block;
+    position: relative;
+    &:hover{
+        transform: scale(1.1);
+    }
+`
 
 const imgs = [kuva1, kuva2, kuva3]
 
@@ -15,16 +25,17 @@ type Props = {
 }
 
 export const ListHomeItem = (props: Props) => {
-    const  { title, pictures } = props.album;
+    const  { title, pictures, slug } = props.album;
 
     const firstImg = pictures?.[0] ?
         formatUrl(picFolder, pictures[0].image) :
         imgs[Math.floor(Math.random() * imgs.length)]
 
-    return (    
+    return ( 
     <ListHomeDetails $bgimg={firstImg}>
+    <LinkStyle to={`/galleria/${slug}`}>
         <h2>{title}</h2>
+        </LinkStyle>
     </ListHomeDetails>
-
     )
 }
